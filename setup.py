759ab903e3afec43967b6c87f95df997f5c3f0a6
Khizer-khan701 <khizerdawood987@gmail.com>
2025-09-25 19:27:43 +0500

    requirements.txt done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages # type: ignore
+setup(
+    name="Speech-to-Text-llm",
+    author="Steam Minds",
+    author_email="smcontentsandprojects024@gmail.com",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[]
+)
